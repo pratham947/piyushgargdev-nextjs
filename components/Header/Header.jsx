03de158786c7b6c16e259d09386a5e8d9b8da1b8
pratham <mehtapratham49@gmail.com>
2023-06-11 16:47:31 +0530

    removed the eztra bracker

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -12,8 +12,13 @@ import { BiLogInCircle } from "react-icons/bi";
 
 import { FaWhmcs } from "react-icons/fa";
 
-import { RiCloseLine, RiYoutubeFill, RiGithubFill, RiTwitterFill, RiLinkedinFill } from "react-icons/ri";
-
+import {
+  RiCloseLine,
+  RiYoutubeFill,
+  RiGithubFill,
+  RiTwitterFill,
+  RiLinkedinFill,
+} from "react-icons/ri";
 
 import { Container } from "reactstrap";
 import { useSession, signOut, signIn } from "next-auth/react";
@@ -94,26 +99,29 @@ const Header = () => {
           <div
             className={`${classes.navigation}`}
             ref={menuRef}
-            onClick={toggleMenu}>
+            onClick={toggleMenu}
+          >
             <div className={`${classes.nav__menu}`}>
-
-              {crossMenu && <div className="border text-white text-3xl  absolute top-10 right-10 font-extrabold">
+              {crossMenu && (
+                <div className="border text-white text-3xl  absolute top-10 right-10 font-extrabold">
                   <RiCloseLine />
                 </div>
               )}
               {NAV__LINK.map((item, index) => (
-
                 <div key={index} className={`${classes.mobile__menuDiv}`}>
                   <Link aria-label={item.display} href={item.path}>
                     <p className={`${classes.mobile__menu}`}>{icons[index]}</p>
                   </Link>
                   <span>{item.display}</span>
                 </div>
-
               ))}
 
               {data && data.user ? (
-                <a className="text-[#808dad] hover:text-[#01d293]" onClick={signOut} href="#">
+                <a
+                  className="text-[#808dad] hover:text-[#01d293]"
+                  onClick={signOut}
+                  href="#"
+                >
                   Sign Out
                 </a>
               ) : (
@@ -126,7 +134,6 @@ const Header = () => {
                 // <a onClick={signIn} href="#">
                 //   Login
                 // </a>
-
               )}
 
               <div className={`${classes.nav__right}`}>
@@ -137,10 +144,9 @@ const Header = () => {
                     target="_blank"
                     title="Youtube Channel"
                     id="youtube-channel"
-
                     className={`ri-youtube-fill cursor-pointer text-white`}
-                    rel="noreferrer">
-
+                    rel="noreferrer"
+                  >
                     <RiYoutubeFill />
                   </Link>
 
@@ -149,10 +155,9 @@ const Header = () => {
                     target="_blank"
                     title="github Account"
                     id="github-account"
-
                     className={`ri-github-line cursor-pointer text-white`}
-                    rel="noreferrer">
-
+                    rel="noreferrer"
+                  >
                     <RiGithubFill />
                   </Link>
 
@@ -161,10 +166,9 @@ const Header = () => {
                     target="_blank"
                     title="Twitter Account"
                     id="twitter-account"
-
                     className={`ri-twitter-fill cursor-pointer text-white`}
-                    rel="noreferrer">
-
+                    rel="noreferrer"
+                  >
                     <RiTwitterFill />
                   </Link>
 
@@ -174,11 +178,10 @@ const Header = () => {
                     title="linkedin Account"
                     id="linkedin-account"
                     className={`ri-linkedin-fill cursor-pointer text-white`}
-                 
-                    rel="noreferrer">
+                    rel="noreferrer"
+                  >
                     <RiLinkedinFill />
                   </Link>
-
                 </p>
               </div>
             </div>
@@ -186,7 +189,8 @@ const Header = () => {
 
           <span
             onClick={() => setCrossMenu(!crossMenu)}
-            className={`${classes.mobile__menu}`}>
+            className={`${classes.mobile__menu}`}
+          >
             <i className="ri-menu-line" onClick={toggleMenu}></i>
           </span>
         </div>
